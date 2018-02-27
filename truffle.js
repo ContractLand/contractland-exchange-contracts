165f9fdeb4ed0f64a562c1cf8d12ab0a8352ee47
Yuming He <yuming.p.he@gmail.com>
2018-02-26 22:33:49 -0500

    Add ropsten testnet config

--- truffle.js
+++ truffle.js
@@ -1,6 +1,9 @@
 require('babel-register');
 require('babel-polyfill');
 
+var HDWalletProvider = require("truffle-hdwallet-provider");
+var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -10,5 +13,12 @@ module.exports = {
       port: 9545,
       network_id: '*', // Match any network id
     },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/KetwbGBGwNagOnjgPUkN")
+      },
+      gas: 4600000,
+      network_id: 3
+    }
   },
 };
