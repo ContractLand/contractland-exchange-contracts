fb08bfecbea173d5637c812436a4587a097835e7
Yuming He <yuming.p.he@gmail.com>
2018-03-03 21:51:39 -0500

    Change send to transfer

--- contracts/CrowdsaleFactory.sol
+++ contracts/CrowdsaleFactory.sol
@@ -19,7 +19,7 @@ contract CrowdsaleFactory is Ownable {
                              address _wallet) payable public returns (address) {
 
         require(msg.value >= cost);
-        owner.send(msg.value);
+        owner.transfer(msg.value);
 
         CrowdsaleToken token = (new CrowdsaleToken(_tokenName, _tokenSymbol));
         SimpleCrowdsale crowdsale = (new SimpleCrowdsale(_openingTime, _closingTime, _rate, _goal, _cap, _wallet, token));
