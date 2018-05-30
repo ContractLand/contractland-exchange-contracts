593cf87f1f4333062b4f8d7cf9dbd4d1d61d43fb
Yuming He <yuming.p.he@gmail.com>
2018-05-29 23:05:02 -0400

    Add proper migration script for exchange, fundstore, and orderbook

--- migrations/2_deploy_exchange.js
+++ migrations/2_deploy_exchange.js
@@ -1,7 +1,26 @@
+var Orderbook = artifacts.require('./Orderbook.sol');
+var FundStore = artifacts.require('./FundStore.sol');
 var Exchange = artifacts.require('./Exchange.sol');
 
 module.exports = function (deployer) {
-  const fundStore = '0x0000000000000000000000000000000000000000'
-  const orderbook = '0x0000000000000000000000000000000000000000'
-  deployer.deploy(Exchange, fundStore, orderbook)
+  deployer.deploy(FundStore)
+  .then(() => {
+    return FundStore.deployed()
+  })
+  .then((fundStoreInstance) => {
+    deployer.deploy(Orderbook)
+    .then(() => {
+      return Orderbook.deployed()
+    })
+    .then((orderbookInstance) => {
+      deployer.deploy(Exchange, fundStoreInstance.address, orderbookInstance.address)
+      .then(() => {
+        return Exchange.deployed()
+      })
+      .then((exchangeInstance) => {
+        fundStoreInstance.updateManager(exchangeInstance.address)
+        orderbookInstance.updateManager(exchangeInstance.address)
+      })
+    })
+  })
 }
