8970f808608754740e6fd2d43ffbb09e1670f30a
H-KE <michael.kehan@gmail.com>
2018-06-23 21:01:25 -0400

    upgrade to latest pragma (0.4.23)

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.23;
 
 import "./ERC20Token.sol";
 import "zeppelin-solidity/contracts/math/SafeMath.sol";
--- contracts/WETH.sol
+++ contracts/WETH.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.23;
 
 contract WETH {
   string public name     = "Wrapped Ether";
