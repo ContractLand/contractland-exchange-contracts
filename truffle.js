ccaf2ed65ba77c4d22c7ae8755bdc3dddda6e5b6
Yuming He <yuming.p.he@gmail.com>
2018-07-07 00:03:28 -0400

    Add ganache setup to allow for deployment of contract over EIP170 limit

--- truffle.js
+++ truffle.js
@@ -13,6 +13,7 @@ module.exports = {
       port: 8545,
       network_id: '*', // Match any network id
       gasPrice: 0,
+      gasLimit: 10000000
     },
     terra: {
       provider: function() {
