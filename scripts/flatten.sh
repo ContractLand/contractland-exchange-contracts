230cf39b667dcf346adf76db25246bbe15fa6336
Yuming He <yuming.p.he@gmail.com>
2018-07-29 23:57:59 -0400

    Update flattener script

--- scripts/flatten.sh
+++ scripts/flatten.sh
@@ -3,4 +3,4 @@
 rm -rf ../flats
 mkdir -p ../flats
 
-../node_modules/.bin/truffle-flattener ../contracts/NewExchange.sol > ../flats/NewExchange_flat.sol
+../node_modules/.bin/truffle-flattener ../contracts/Exchange.sol > ../flats/Exchange_flat.sol
