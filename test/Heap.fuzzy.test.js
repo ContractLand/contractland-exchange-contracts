36e62837e99e68464b5ec2a46220f97628540ed9
Yuming He <yuming.p.he@gmail.com>
2018-12-04 22:56:42 -0500

    Tiddy up

--- test/Heap.fuzzy.test.js
+++ test/Heap.fuzzy.test.js
@@ -1,15 +1,14 @@
 const TestAskHeap = artifacts.require("TestAskHeap")
 const TestBidHeap = artifacts.require("TestBidHeap")
 
-contract.only('Heap Fuzzy Test',  async(accounts) => {
-  it.only("AskHeap", async() => {
+contract('Heap Fuzzy Test',  async(accounts) => {
+  it("AskHeap", async() => {
     const askHeap = await TestAskHeap.new()
-    const testSize = 10
+    const testSize = 100
     let min, oldMin, size
 
     let vals = Array.from({length: testSize}, () => Math.floor(Math.random() * 100000))
 
-    console.log(vals)
     size = await askHeap.size.call()
     assert.equal(size.toNumber(), 0)
 
@@ -27,7 +26,6 @@ contract.only('Heap Fuzzy Test',  async(accounts) => {
       await askHeap.pop()
       oldMin = min
       min = await askHeap.peak.call()
-      console.log(oldMin[4].toNumber(), min[4].toNumber())
       assert.isTrue(oldMin[4].toNumber() <= min[4].toNumber())
     }
 
@@ -45,7 +43,7 @@ contract.only('Heap Fuzzy Test',  async(accounts) => {
     size = await bidHeap.size.call()
     assert.equal(size.toNumber(), 0)
 
-    for (let i = 1; i < vals.length; i++) {
+    for (let i = 0; i < vals.length; i++) {
       await bidHeap.add(0, 0, 0, 0, vals[i], 0, 0)
     }
 
